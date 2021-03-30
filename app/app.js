2f5c3df11387a3374740b773d628f50d900785f7
johnseydak <63112275+johnseydak@users.noreply.github.com>
2021-03-30 00:54:31 -0400

    update

---
+++ app/app.js
@@ -0,0 +1,80 @@
+var userVarName = "Variable";
+var homeContent =`<div class="home">
+<div class="hero1"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+<div class="container1">
+    <div class="image1"></div>
+    <div class="text1">
+        <h1>Hello</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. </p>
+    </div>
+</div>
+<div class="container2">
+    <div class="text2">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. </p>
+    </div>
+    <div class="image2"></div>
+</div>
+</div>`;             //home in homeContent matches the nav home id
+var aboutContent =`<div class="about">
+<div class="image3"></div>
+<div class="text3">
+    <h1>About us...</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p>
+</div>
+<div class="image4"></div>
+<div class="text4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+<div class="image5"></div>
+<div class="text6"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+<div class="text7"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+</div>`;
+var locationsContent =`<div class="locations">
+<div class="tours"><h1>Tours</h1></div>
+<div class="column1">
+    <div class="image6"></div>
+    <div class="text8"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+    <div class="image7"></div>
+    <div class="text9"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+</div>
+<div class="column2">
+    <div class="image8"></div>
+    <div class="text10"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+    <div class="text11"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+    <div class="image9"></div>
+    <div class="text12"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p></div>
+</div>
+</div>`;
+var contactContent =`<div class="contact">
+<h1>CONTACT</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tincidunt mi non sem dapibus, convallis lobortis erat consectetur. Praesent finibus nisi eu bibendum tristique. Nullam magna nulla, feugiat vitae iaculis id, bibendum nec ligula. Nullam id enim mattis, convallis orci quis, hendrerit velit. Suspendisse tortor tortor, cursus sed libero sed.</p>
+</div>`;
+
+//this is an example of a named function
+function init(){
+    //this is where the instructions go ..alert stops site, console log keeps it going
+    //this is a click listener that will get the link id
+    $("nav a").click(function (e) {
+        //let btnId = e.currentTarget.id;  this worked to get link
+        let btnId = this.id;
+        let contentID = btnId + "Content";
+
+        $("#app").html(eval(contentID)); //# is the id, app is the div that is in the html page, .html will erase and write over whatever is inside of the app div (btnID or contentID is what is being referenced) eval will take the above line and mix it into an ID
+        //$("#app").append(btnId); //will add to the end whatever btnId is clicked on to the end, after the H1
+        //$("#app").prepend(contentID); //will add whatever btnId is clicked on BEFORE the H1
+
+        //console.log("clicked ", this.id);  //doing console.log("clicked " + this); will tell you what you clicked on
+    }); 
+    //compound selector goes to nav and listens for the anchor tag   .click is the event
+    //console.log("init here " + userName); 
+    //alert("init " + userName);
+}
+
+
+//this checks for all of the elements to be read by the browser
+$(document).ready(function() {
+    init();           //if you don't want the JS to run, you can just comment out this init
+});
+
+//left off at video timestamp 1:40:00
